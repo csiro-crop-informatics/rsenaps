0639313b847e0168bc47ddc8fe8580655ad89753
Alex Whan <alexwhan@gmail.com>
2020-08-05 12:40:28 +1000

    using .onLoad to set apikey if available or give useful message. #13

---
+++ R/zzz.R
@@ -0,0 +1,19 @@
+.onLoad <- function(libname, pkgname) {
+
+    senaps.local <- local_senaps()
+    if(senaps.local & !is.null(Sys.getenv("SENAPS_APIKEY"))) {
+        senaps_options(apikey = Sys.getenv("SENAPS_APIKEY"))
+        message("RSenaps is running locally and the API key has been set to the environment variable SENAPS_APIKEY\n")
+    } else {
+        if(senaps.local) {
+            message(
+"RSenaps is running locally, but the API key is not set.\n
+See ?senaps_options for help on setting the API key.\n")
+        } else {
+            message(
+"If you are running RSenaps locally, use local_senaps(set_local = TRUE), or set an
+environment variable LOCAL_SENAPS=TRUE in a .Renviron file.\n
+See ?senaps_options for help on setting the API key.\n")
+        }
+    }
+}
