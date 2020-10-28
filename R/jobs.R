246b2099915ef401b0b93c756a69252727909dff
Bangyou Zheng <zheng.bangyou@gmail.com>
2020-10-28 17:06:50 +1100

    Get job logs
    (cherry picked from commit bf88d9a2fc427e0c6f87cf9a0affd5dd89d93cd2)

--- R/jobs.R
+++ R/jobs.R
@@ -5,10 +5,11 @@
 #'
 #' @param workflowid Workflow id
 #' @param limit Number of jobs retrieved
+#' @param skip Skip the number of jobs for pagination
 #'
 #' @return A list of jobs
 #' @export
-get_jobs <- function(workflowid = NULL, limit = NULL) {
+get_jobs <- function(workflowid = NULL, limit = NULL, skip = NULL) {
 
     query <- list()
     if (!is.null(workflowid)) {
@@ -27,11 +28,55 @@ get_jobs <- function(workflowid = NULL, limit = NULL) {
         query$limit <- limit
     }
 
+
+    if (!is.null(skip)) {
+        if (length(skip) > 1) {
+            stop('Only a single value for skip')
+        }
+        if (!is.numeric(skip)) {
+            stop("Numeric value is required.")
+        }
+        query$skip <- skip
+    }
+
     response <- request(GET, 'jobs', query = query)
     httr::stop_for_status(response)
     response <- httr::content(response)
-
-    jobs <- response$`_embedded`$jobs
-    jobs
+    response
 }
 
+
+#' Get logs for a jog
+#'
+#' @param jobid The job id
+#' @param limit Number of logs retrieved
+#'
+#' @return  A list for job
+#' @export
+get_job_log <- function(jobid, limit = NULL) {
+    if (!is.null(jobid)) {
+        if (length(jobid) > 1) {
+            stop('Only a single value for jobid')
+        }
+        if (!is.character(jobid)) {
+            stop("character value is required.")
+        }
+    }
+
+    url <- paste0('jobs', "/", jobid, "/log")
+    query <- list()
+    if (!is.null(limit)) {
+        if (length(limit) > 1) {
+            stop('Only a single value for limit.')
+        }
+        if (!is.numeric(limit)) {
+            stop("Numeric value is required.")
+        }
+        query$limit <- limit
+    }
+
+    response <- request(httr::GET, url, query = query)
+    .stop_for_status(response)
+    response <- httr::content(response)
+    response
+}
